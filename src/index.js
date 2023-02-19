d3721f42a3a4dfab28c3933b9de7cd6aef60efc5
Eleen-228 <109869625+Eleen-228@users.noreply.github.com>
2023-02-18 19:48:22 -0500

    change Router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './App.css'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename="/Daily_Planner_App_ReactVersion">
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 )
